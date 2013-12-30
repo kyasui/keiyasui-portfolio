6f6ecdeb3b721460d1b06a933b963e496015e8e5
Kei Yasui <kyasui@Keis-MacBook-Air.local>
2013-12-30 14:47:12 -0500

    latest

--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,12 @@ var k = {
 		w_width: 0
 	},
 	ui: {
+		$doc: $(document),
 		$w: $(window),
+		$body: $('body'),
 		$all_sections: $('.section'),
 		$intro_section: $('.intro-section'),
+		$projects_section: $('#projects-section'),
 		$work_rows: $('.work-row')
 	},
 	init: function() {
@@ -30,8 +33,10 @@ var k = {
 				desc_height = $work_desc.height();
 				img_height = $this.find('.work-img').height();
 
-			if (k.vars.w_width >= 728) {
+			if (k.vars.w_width >= 960) {
+				$this.height('auto');
 				$work_item.height(img_height);
+				$work_desc.css('top', '');
 			} else {
 				$this.height(img_height + desc_height);
 				$work_item.height(img_height + desc_height);
@@ -40,12 +45,16 @@ var k = {
 		});
 	},
 	init_bg_media: function() {
-		var bg_media = new $.BigVideo();
-		bg_media.init();
-		bg_media.show('/video/mountain.m4v', {
-			ambient: true,
-			altSource: '/video/mountain.ogv'
-		});
+		if (k.vars.w_width >= 960) {
+			var bg_media = new $.BigVideo();
+			bg_media.init();
+			bg_media.show('/video/city.mp4', {	
+				ambient: true,
+				altSource: '/video/city.ogv'
+			});
+		} else {
+			k.ui.$body.addClass('mobile');
+		}
 	},
 	init_waypoints: function() {
 		k.ui.$all_sections.each(function(){
@@ -65,7 +74,7 @@ var k = {
 		});
 	},
 	bind_events: function() {
-		$(document).on('click', '.nav-trigger', function(e){
+		k.ui.$doc.on('click', '.nav-trigger', function(e){
 			e.preventDefault();
 
 			var $this = $(this),
@@ -90,6 +99,33 @@ var k = {
 					}, 100*index);					
 				});
 			}
+		});
+
+		k.ui.$projects_section.imagesLoaded().always( function( instance ) {
+		    k.ui.$w.trigger('resize');
+		}).done( function( instance ) {
+		    // console.log('all images successfully loaded');
+		}).fail( function() {
+		    // console.log('all images loaded, at least one is broken');
+		}).progress( function( instance, image ) {
+		    $(image.img).parents('.work-image').addClass('loaded');
+		});
+
+		k.ui.$doc.on('click', '.nav-item', function(e){
+			e.preventDefault();
+
+            var $this = $(this),
+            	$nav_items = $('.nav-item'),
+                target = $this.attr('href'),
+                target_offset = $(target).offset().top;
+
+            $nav_items.removeClass('selected');
+
+            $this.addClass('selected');
+
+            $('html, body').animate({
+                scrollTop:target_offset
+            }, 500);
 		});
 
 		k.ui.$w.on('resize', function(){
